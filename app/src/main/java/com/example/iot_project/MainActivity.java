54eb3978bf40a4e73fdf1970407ef2f6e1cd96a0
Mahammad Mammadov <mama9087@student.su.se>
2023-12-12 23:53:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/iot_project/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.iot_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MqttAndroidClient client;
+    private static final String SERVER_URI = "tcp://172.20.10.2:1883";
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Connecting to....");
+        connect();
+    }
+
+    private void connect(){
+        String clientId = MqttClient.generateClientId();
+        client =
+                new MqttAndroidClient(this.getApplicationContext(), SERVER_URI,
+                        clientId);
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                    System.out.println(TAG + " Success. Connected to " + SERVER_URI);
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception)
+                {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+                    System.out.println(TAG + " Oh no! Failed to connect to " +
+                            SERVER_URI);
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
